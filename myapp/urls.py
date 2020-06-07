089a9cdcd152d7d7f1269309b23803c1c1811e93
HOD101s <manasbass.99@gmail.com>
2020-06-07 18:23:59 +0530

    added craigslist scraper

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name='home'),
-	# path('new_search', views.new_search, name='new_search'),
+	path('new_search', views.new_search, name='new_search'),
 ]
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,44 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
+
+cglURL = "https://mumbai.craigslist.org/search/?query={0}"
 
 def home(request):
 	return render(request,'base.html')
+
+def new_search(request):
+	search = request.POST.get('search')
+	resp = requests.get(cglURL.format(search))
+	soup = BeautifulSoup(resp.text, features='html.parser')
+
+	post_listings = soup.find_all('li', {'class': 'result-row'})
+
+	final_postings = list()
+
+	print(post_listings[0])
+
+	for post in post_listings:
+		post_title = post.find(class_='result-title').text
+		post_url = post.find('a').get('href')
+
+		if post.find(class_='result-price'):
+			post_price = post.find(class_='result-price').text
+		else:
+			post_price = 'N/A'
+
+		if post.find('img'):
+			post_image_url = post.find('img')['src']
+			print(post_image_url)
+		else:
+			post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+		final_postings.append((post_title, post_url, post_price, post_image_url))
+
+	context = {
+		'search': search,
+		'final_postings': final_postings,
+	}
+	return render(request,'myapp/new_search.html',context)
 
 # Create your views here.
